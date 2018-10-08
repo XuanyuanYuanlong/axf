8ad9c5e4d2628f15880c3e0ec7b5618f7fdac082
XuanyuanYuanlong <xuanyuanlongze@163.com>
2018-10-08 21:21:20 +0800

    1

--- axf/settings.py
+++ axf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'olamcr4a$6ig)h7dm9meot(z7&r!m!-6i_x^k(jf5i)^)5--7l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'axfApp',
 ]
 
 MIDDLEWARE = [
